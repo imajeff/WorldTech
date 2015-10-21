110022c48295d6fa06210df7e8cd9d85255b3460
Jeff Smith <imajeffs@hotmail.com>
2015-10-21 08:49:27 -0600

    adds buttons
    Finished so each button changes bg color. Selecting a color and pressing
    "add new" correctly adds a button for that color.
    Apparently we're ok that it adds even if the color button was already
    added.

--- color_changer/src/main.js
+++ color_changer/src/main.js
@@ -1,26 +1,189 @@
-var btnLabels = ["Red","Green","Blue","Purple","Brown","Orange","Yellow","Gray"];
+
+function addButton(color) {
+  txt = document.createTextNode(color);
+  // Add button according to color text
+  ele = document.createElement("button");
+  ele.value = color.toLowerCase();
+  ele.setAttribute('onclick','changeBG(this.value)');
+  ele.appendChild(txt);
+  document.getElementById("color-buttons").appendChild(ele);
+}
+
+function addOption(color) {
+  txt = document.createTextNode(color);
+  ele = document.createElement("option");
+  ele.setAttribute('value',color.toLowerCase());
+  // Add option
+  ele.appendChild(txt);
+  document.getElementById("color-options").appendChild(ele);
+}
+
+function changeBG(color) {
+  document.body.style.backgroundColor = color;
+  console.log('color changed '+document.body.style.backgroundColor);
+}
+
+function getSelectedColor() {
+  // what color currently in the selection
+  return document.getElementById('color-options').value;
+}
 
 function go() {
-  var btnParent = document.getElementById("color-buttons");
-  var optionParent = document.getElementById("color-options");
-  var ele;
-  var txt;
   var initialBtnCnt = 3;
-  var i;
 
   // Add initial color buttons
-  for(i=0; i<btnLabels.length; i++) {
-    console.log(btnLabels[i]);
-    var txt = document.createTextNode(btnLabels[i]);
-    // Add option
+  for(var i=0; i<btnLabels.length; i++) {
+    var color = btnLabels[i];
+    console.log(color);
 
     if(i < initialBtnCnt) {
-      // Add button
+      // initially add only first few buttons
+      //console.log('(button too)');
+      addButton(color);
     }
-    var btn = document.createElement("button");
-    ele.appendChild(txt);
-    parent.appendChild(ele);
+
+    addOption(color);
   }
 }
 
 go();
+
+var btnLabels = ["Red","Green","Blue",
+'Black',
+'Navy',
+'DarkBlue',
+'MediumBlue',
+'DarkGreen',
+'Teal',
+'DarkCyan',
+'DeepSkyBlue',
+'DarkTurquoise',
+'MediumSpringGreen',
+'Lime',
+'SpringGreen',
+'Aqua',
+'Cyan',
+'MidnightBlue',
+'DodgerBlue',
+'LightSeaGreen',
+'ForestGreen',
+'SeaGreen',
+'DarkSlateGray',
+'LimeGreen',
+'MediumSeaGreen',
+'Turquoise',
+'RoyalBlue',
+'SteelBlue',
+'DarkSlateBlue',
+'MediumTurquoise',
+'Indigo',
+'DarkOliveGreen',
+'CadetBlue',
+'CornflowerBlue',
+'RebeccaPurple',
+'MediumAquaMarine',
+'DimGray',
+'SlateBlue',
+'OliveDrab',
+'SlateGray',
+'LightSlateGray',
+'MediumSlateBlue',
+'LawnGreen',
+'Chartreuse',
+'Aquamarine',
+'Maroon',
+'Purple',
+'Olive',
+'Gray',
+'SkyBlue',
+'LightSkyBlue',
+'BlueViolet',
+'DarkRed',
+'DarkMagenta',
+'SaddleBrown',
+'DarkSeaGreen',
+'LightGreen',
+'MediumPurple',
+'DarkViolet',
+'PaleGreen',
+'DarkOrchid',
+'YellowGreen',
+'Sienna',
+'Brown',
+'DarkGray',
+'LightBlue',
+'GreenYellow',
+'PaleTurquoise',
+'LightSteelBlue',
+'PowderBlue',
+'FireBrick',
+'DarkGoldenRod',
+'MediumOrchid',
+'RosyBrown',
+'DarkKhaki',
+'Silver',
+'MediumVioletRed',
+'IndianRed',
+'Peru',
+'Chocolate',
+'Tan',
+'LightGray',
+'Thistle',
+'Orchid',
+'GoldenRod',
+'PaleVioletRed',
+'Crimson',
+'Gainsboro',
+'Plum',
+'BurlyWood',
+'LightCyan',
+'Lavender',
+'DarkSalmon',
+'Violet',
+'PaleGoldenRod',
+'LightCoral',
+'Khaki',
+'AliceBlue',
+'HoneyDew',
+'Azure',
+'SandyBrown',
+'Wheat',
+'Beige',
+'WhiteSmoke',
+'MintCream',
+'GhostWhite',
+'Salmon',
+'AntiqueWhite',
+'Linen',
+'LightGoldenRodYellow',
+'OldLace',
+'Fuchsia',
+'Magenta',
+'DeepPink',
+'OrangeRed',
+'Tomato',
+'HotPink',
+'Coral',
+'DarkOrange',
+'LightSalmon',
+'Orange',
+'LightPink',
+'Pink',
+'Gold',
+'PeachPuff',
+'NavajoWhite',
+'Moccasin',
+'Bisque',
+'MistyRose',
+'BlanchedAlmond',
+'PapayaWhip',
+'LavenderBlush',
+'SeaShell',
+'Cornsilk',
+'LemonChiffon',
+'FloralWhite',
+'Snow',
+'Yellow',
+'LightYellow',
+'Ivory',
+'White'];
