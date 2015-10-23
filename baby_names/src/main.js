a141f4512dd0a1c4d546843bfa15ceebf9340212
Jeff Smith <imajeffs@hotmail.com>
2015-10-22 21:19:05 -0600

    started baby names
    side note: suggestion to fix bug in color_changer that adds blank button
    if have not selected a color first.

---
+++ baby_names/src/main.js
@@ -0,0 +1,10 @@
+
+function go() {
+  // Set Title of this assignment
+  var assn = 'Baby Name Generator';
+  document.title = assn;
+  document.getElementById('title').innerHTML = assn;
+  
+}
+
+go();
