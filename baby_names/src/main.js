e3cc3804c4f0dbd2c63b03c275495c706f303208
Jeff Smith <imajeffs@hotmail.com>
2015-10-25 12:51:52 -0600

    need get value from lname field

--- baby_names/src/main.js
+++ baby_names/src/main.js
@@ -1,5 +1,13 @@
 var assn = 'Baby Name Generator';
 
+function generateName() {
+  var fname = "jefferson";
+  var mname = "lee";
+  var lname = document.
+  var txt = document.createTextNode(fname+" "+mname+" "+lname);
+  var out = document.getElementById("baby_name");
+}
+
 function go() {
   // Set Title of this assignment
   document.title = assn;
