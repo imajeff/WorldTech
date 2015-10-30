35779c3ccb980ebe30ffeaa82828dbe16acd4a2d
Jeff Smith <imajeffs@hotmail.com>
2015-10-29 21:19:47 -0600

    todo list crosses off and uncrosses

--- todo_list/src/main.js
+++ todo_list/src/main.js
@@ -1,7 +1,35 @@
 var assn = 'ToDo List';
 
+function crossOffItem(item) {
+    //console.log('-'+item.className+'-');
+    if(item.className.length=='') {
+        item.className = 'crossed';
+    }
+    else {
+        item.className = '';
+    }
+}
+
+function addItem(str) {
+    var out = document.getElementById('list_out');
+    var ele = document.createElement('li');
+    ele.setAttribute("onclick","crossOffItem(this)");
+    ele.appendChild(document.createTextNode(str));  //doesn't use markup
+    out.appendChild(ele);
+}
+
 function updateList() {
     var form = document.forms['todo_form'];
+    var entry = form.entry.value;
+    if(entry.length==0) return;     //if entry blank, abort
+    var entries = entry.split(',');
+    console.log(entries);
+    // Parse by commas and add each item
+    for (var i=0; i < entries.length; i++) {
+        addItem(entries[i].trim());
+    }
+    // reset input field
+    form.entry.value = '';
 }
 
 function go() {
