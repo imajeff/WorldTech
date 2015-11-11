603999b1a4f7c76b09388ef69fd3804c2468a437
Jeff Smith <imajeffs@hotmail.com>
2015-11-10 21:09:28 -0700

    started MadLibs

---
+++ MadLib/src/main.js
@@ -0,0 +1,38 @@
+var assn = 'Mad Libs';
+var types = ['@ADJECTIVE','@NOUN','@ADVERB','@VERB'];
+var defAdjs = ['yellow','giant','phony','pretty','crusty','selfish','careful','slothful','green','pukey'];
+var defNouns = ['rock','car','person','bouncy ball','package','penguin','whisker','bullet','place','example'];
+var defAdvs = ['shyly','slothfully','completely','grudgingly','abruptly','inadvertently','coyly','accidentally','awkwardly','never'];
+var defVerbs = ['run','slaughter','caress','impale','enhance','permeate','climb','crawl'];
+var lstAdjs;
+var lstNouns;
+var lstAdvs;
+var lstVerbs;
+
+function getWord(type) {
+    // Convert lbl to a random word
+    switch (lbl) {
+        case types[0]:
+        //adj
+        adjWords[];
+        break;
+        case types[1]:
+        //noun
+        break;
+        case types[2]:
+        //adv
+        break;
+        case types[3]:
+        //verb
+        break;
+    }
+}
+
+function go() {
+    // Set Title of this assignment
+    document.title = assn;
+    document.getElementById('title').innerHTML = assn;
+
+}
+
+go();
