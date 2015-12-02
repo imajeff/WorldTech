422c7285c69ced408c63f3b8540305bf91124221
Jeff Smith <imajeffs@hotmail.com>
2015-12-01 21:04:43 -0700

    class Tue 12/1

---
+++ hybrid_normal/src/main.js
@@ -0,0 +1,10 @@
+var assn = 'Hybrid vs Normal';
+
+
+function go() {
+    // Set Title of this assignment
+    document.title = assn;
+    document.getElementById('title').innerHTML = assn;
+}
+
+go();
