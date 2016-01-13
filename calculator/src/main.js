e7dba5963018240c1ac8d58e46133a99864dab2b
Jeff Smith <imajeffs@hotmail.com>
2016-01-12 21:10:48 -0700

    class 1/12
    jQuery works, need layout calculator components on page

---
+++ calculator/src/main.js
@@ -0,0 +1,20 @@
+var assn = 'Calculator';
+var calcResult;
+
+function enterKey() {
+    calcResult = '2 + 3' +' * '+Math.random()*6;
+    calcResult += ' = ' + eval(calcResult).toFixed(3);
+    console.log(calcResult);
+}
+
+function go() {
+    // Set Title of this assignment
+    document.title = assn;
+    document.getElementById('title').innerHTML = assn;
+
+}
+
+$(document).ready(function(){
+    go();
+});
+
