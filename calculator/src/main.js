6c6bbcb494f6f681c84adda7963d797ed8cf44dd
Jeff Smith <imajeffs@hotmail.com>
2016-01-20 20:47:22 -0700

    library

--- calculator/src/main.js
+++ calculator/src/main.js
@@ -1,18 +1,17 @@
 var assn = 'Calculator';
 var calcResult;
-
-$('button:submit').click(function() {
-    calcResult = '2 + 3';// +' * '+Math.random()*6;
-    calcResult += ' = ' + eval(calcResult).toFixed(3);
-    // update calculation string in the text field
-    $('input:text').val(calcResult);
-    console.log(calcResult);
-    return false;
-});
+//TODO https://gist.github.com/johndavedecano/3149185
 
 $(document).ready(function(){
     // Set Title of this assignment
     document.title = assn;
     document.getElementById('title').textContent = assn;
+
+    $('button:submit').click(function() {
+    	calcResult += ' = ' + eval( $("").val() );
+    	// update calculation string in the text field
+    	$('#exp').val(calcResult);
+    	console.log(calcResult);
+	});
 });
 
