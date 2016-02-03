1c32c755e0fb470b9746bc12bd34428c96f69b38
Jeff Smith <imajeffs@hotmail.com>
2016-02-02 20:53:16 -0700

    equals key worked with jquery but something broke

--- calculator/src/main.js
+++ calculator/src/main.js
@@ -2,15 +2,18 @@ var assn = 'Calculator';
 var calcResult;
 //TODO https://gist.github.com/johndavedecano/3149185
 
+
 $(document).ready(function(){
     // Set Title of this assignment
     document.title = assn;
     document.getElementById('title').textContent = assn;
 
-    $('#1 #2 #3').click(function() {
-    	calcResult += ' = ' + eval( $("").val() );
+
+    $('#exe').click(function(){
+    	// get answer
+    	calcResult = eval($('#eval').val());
     	// update calculation string in the text field
-    	$('#exp').val(calcResult);
+    	$('#ans').val(calcResult);
     	console.log(calcResult);
 	});
 });
