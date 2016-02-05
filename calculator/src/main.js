60f16e19cd5b1c997adb68bbe80d6bd5f9c9b570
Jeff Smith <imajeffs@hotmail.com>
2016-02-04 17:12:38 -0700

    equals works but bug appending key text

--- calculator/src/main.js
+++ calculator/src/main.js
@@ -1,20 +1,28 @@
 var assn = 'Calculator';
-var calcResult;
 //TODO https://gist.github.com/johndavedecano/3149185
 
+function doEval() {
+    // get answer
+    var calcResult = eval($('#eval').val());
 
-$(document).ready(function(){
+    // update calculator output
+    $('#ans').text(calcResult);
+    console.log(calcResult);
+}
+
+$(function() {
     // Set Title of this assignment
     document.title = assn;
     document.getElementById('title').textContent = assn;
 
+    var input =  $('#eval');
+    var answer = $('#ans');
 
-    $('#exe').click(function(){
-    	// get answer
-    	calcResult = eval($('#eval').val());
-    	// update calculation string in the text field
-    	$('#ans').val(calcResult);
-    	console.log(calcResult);
-	});
+    $('#exe').click(doEval);
+
+    $('#0,#1').click(function() {
+        // append key's text to input evaluation string
+        input.append(this.text());
+    });
 });
 
